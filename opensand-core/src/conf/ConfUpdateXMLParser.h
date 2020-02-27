b711d50b4bf44a95dab9874bb280df24ccb79a5d
Nicolas Savatier <savatier.nicolas@gmail.com>
2020-02-27 13:18:34 +0100

    CLEAN : Factorized ConfUpdateXMLParsers
    Factorized the common code of the 2 parsing functions into a single one.

--- opensand-core/src/conf/ConfUpdateXMLParser.h
+++ opensand-core/src/conf/ConfUpdateXMLParser.h
@@ -24,10 +24,47 @@ public:
 
 	~ConfUpdateXMLParser();
 
+	/**
+	 * Modifies the bandwidth value of the forwardLink in the /etc/opensand/core_global.conf configuration file.
+	 * This function parses the XML file looking for the tag with attribute "id" = spot and "gw" = gw_id
+	 * if it finds said tag, it odifies its value to newValue, and returns true.
+	 * If the tag is not found or any error occurs, the function returns false.
+	 * @param spot id of the spot for which we want to modify the bandwidth in the configuration file
+	 * @param gw_id id of the gateway for which we want to modify the bandwidth in the configuration file
+	 * @param newValue new value of the bandwidth to be written
+	 * @return true on success, else false
+	 */
     bool modifyForwardBandwidthInGlobalConf(spot_id_t spot, tal_id_t gw_id, freq_mhz_t newValue);
 
+    /**
+	 * Modifies the bandwidth value of the returnLink in the /etc/opensand/core_global.conf configuration file.
+	 * This function parses the XML file looking for the tag with attribute "id" = spot and "gw" = gw_id
+	 * if it finds said tag, it odifies its value to newValue, and returns true.
+	 * If the tag is not found or any error occurs, the function returns false.
+	 * @param spot id of the spot for which we want to modify the bandwidth in the configuration file
+	 * @param gw_id id of the gateway for which we want to modify the bandwidth in the configuration file
+	 * @param newValue new value of the bandwidth to be written
+	 * @return true on success, else false
+	 */
     bool modifyReturnBandwidthInGlobalConf(spot_id_t spot, tal_id_t gw_id, freq_mhz_t newValue);
 
+private:
+
+    /**
+	 * Modifies the bandwidth value contained in rootTag in the /etc/opensand/core_global.conf configuration file.
+	 * This function parses the XML file looking for rootTag,
+     * then parses its children, looking the tag with attribute "id" = spot and "gw" = gw_id
+	 * if it finds said tag, it odifies its value to newValue, and returns true.
+	 * If the tag is not found or any error occurs, the function returns false.
+     *
+     * Basically, this function factorizes the code in common between the two functions above (ie 99.9% of their code).
+     *
+	 * @param spot id of the spot for which we want to modify the bandwidth in the configuration file
+	 * @param gw_id id of the gateway for which we want to modify the bandwidth in the configuration file
+	 * @param newValue new value of the bandwidth to be written
+	 * @return true on success, else false
+	 */
+    bool modifyBandwidthInGlobalConf(spot_id_t spot, tal_id_t gw_id, freq_mhz_t newValue, string rootTag);
 };
 
 
